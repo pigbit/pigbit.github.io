57ffbce560e5f1dfed4740236d5157d480bb3b32
pigbit <47769972+pigbit@users.noreply.github.com>
2019-05-08 09:05:11 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,1 @@
-window.onload = setInterval(function () {
-    document.getElementsByTagName("h1")[0].style.color = "rgb(" + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ")";
-    document.getElementsByTagName("h1")[0].style.borderColor = "rgba(" + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + "," + Math.random() + ")";
-}, 500);
+
